ecd2ab39aca7e7805f1396739592a2662a175b1a
uzair alam <uzair.alam@venturedive.com>
2019-10-14 13:00:25 +0500

    Remove unncessary comments and block

--- server/boot/root.js
+++ server/boot/root.js
@@ -10,40 +10,5 @@ module.exports = function(server) {
   const baseUrl = server.get('restApiRoot');
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-
-  // router.get(`${baseUrl}/events`,async (req, res) => {
-  //   server.models.NotificationSettings.find({}, (err, notifications) => {
-  //     console.log(notifications);
-  //     res.send({status: true, notifications});
-  //   });
-  // });
-
-  // router.get(`${baseUrl}/events/:event_id`,async (req, res) => {
-  //   server.models.NotificationSettings.find({}, (err, notifications) => {
-  //     console.log(notifications);
-  //     res.send({status: true, notifications});
-  //   });
-  // });
-
-  // router.get(`${baseUrl}/notifications/:user_id`,async (req, res) => {
-  //   server.models.NotificationSettings.find({}, (err, notifications) => {
-  //     console.log(notifications);
-  //     res.send({status: true, notifications});
-  //   });
-  // });
-
-  // router.get(`${baseUrl}/notifications/:user_id`,async (req, res) => {
-  //   server.models.NotificationSettings.find({}, (err, notifications) => {
-  //     console.log(notifications);
-  //     res.send({status: true, notifications});
-  //   });
-  // });
-
-  // router.get(`${baseUrl}/notifications/:user_id`,async (req, res) => {
-  //   server.models.NotificationSettings.find({}, (err, notifications) => {
-  //     console.log(notifications);
-  //     res.send({status: true, notifications});
-  //   });
-  // });
   server.use(router);
 };
